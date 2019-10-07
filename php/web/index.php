c2e7fd349a1ded23ef4568595c2c585942d4ad35
Gonzalo Andrés Garcia Cartagena <gonzalo.garcia@ceiba.com.co>
2019-10-06 23:09:30 -0500

    se añade html a sitio php

--- php/web/index.php
+++ php/web/index.php
@@ -1,3 +1,15 @@
-<?php
-    echo gethostname();
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Nombre del host</title>
+    </head>
+    <body>
+        <h1>El nombre del host es 
+            <?php
+                echo gethostname();
+            ?>
+        </h1>
+        
+    </body>
+</html>
